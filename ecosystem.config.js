722f63eb0bb64f96b5fff8af0084ee50feac75ca
Kangketik <info@kangketik.web.id>
2021-02-17 00:04:06 +0700

    :zap: Kayaknya ready

---
+++ ecosystem.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  apps: [{
+    name: "Express Environment",
+    script: 'index.js',
+    watch: '.',
+    env: {
+      "NODE_ENV": "development",
+    },
+    env_production: {
+      "NODE_ENV": "production"
+    },
+    error_file: 'logs/err.log',
+    out_file: 'logs/out.log',
+    log_file: 'logs/combined.log',
+    time: true
+  }],
+
+  deploy: {
+    production: {
+      user: 'akasakaryu',
+      host: '151.106.113.4',
+      port: '14045',
+      ref: 'origin/main',
+      repo: 'git@github.com:kangketikonlen/express-env.git',
+      path: '/home/akasakaryu/Projects/Express-ENV',
+      'pre-deploy-local': '',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
